af4fcb26add574a9df8f9734501a8968acaf1083
Todd Moody <moods@Todds-MacBook-Pro.local>
2018-02-12 21:23:26 +1300

    add Webpack to compile and minify js

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    entry: ['./js/markerclusterer.js', './js/main.js'],
+    output: {
+      filename: './js/build.js',
+      path: __dirname
+    },
+    watch: false,
+    module: {
+        rules: [
+          {
+            test: /\.js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env']
+              }
+            }
+          }
+        ]
+      }
+  };
